9304849d3ce89764e28f215d600e63f4454d7c4c
Marty Hernandez <margarito.hernandezc@gmail.com>
2022-05-02 01:15:29 -0700

    questions and choice display

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// when I click, timer starts, show questions
+//when question is answered, show next question
+//when user answers question incorrect, subtract time from timer
+//when no questions left or timer is 0, game over
+//user can save initials and score(time)
+
+var startButton = document.querySelector(".start-button");
+var timerElement = document.querySelector("#currentTime");
+var quizElement = document.querySelector("#quiz");
+var questionList = document.getElementById("questionlist");
+var quizHeader = document.getElementById("quizHeader");
+var quizInfo = document.getElementById("quizInfo");
+var choiceEl = document.getElementById("choices");
+var infoEl = document.querySelector(".questionInfo");
+
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -26,14 +41,90 @@ var questions = [
     },
 
 ];
-var score = 0;
 
-for (var i = 0; i < questions.length; i++);
-var questionEl = questions.title;
-var choiceEl = questions.choices;
-if (choiceEl == questions[i].answer) {
-    score++;
-    alert("Correct");
-} else {
-    alert("Wrong");
+var score = 0;
+var timer;
+var questionIndex = 0;
+var timerCount = 75;
+var questionList = document.createElement("ul");
+
+
+//function to start the game on button click
+function startGame() {
+    // isWin = false;
+
+    // startTimer()
+    console.log("Start the game");
+    quizHeader.style.display = "none";
+    quizInfo.style.display = "none";
+    startButton.style.display = "none";
+
+    showQuestions();
 }
+
+//timer function starts the timer
+
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (isWin && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                winGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+        }
+    }, 1000);
+}
+
+
+
+//function to show cycle questions
+function showQuestions() {
+
+    if (questionIndex < questions.length) {
+
+        var questionObject = questions[questionIndex];
+        var userQuestion = questionObject.title;
+        infoEl.innerText = userQuestion;
+
+        var userChoices = questionObject.choices;
+        choiceEl.innerHTML = "";
+
+        for (var i = 0; i < userChoices.length; i++) {
+            var choiceText = userChoices[i];
+            var choiceButton = document.createElement("button");
+            choiceButton.innerText = choiceText;
+            choiceButton.classList.add("btn", "btn-info");
+            choiceEl.appendChild(choiceButton);
+        }
+        questionIndex++
+    } else {
+        gameOver()
+
+    }
+
+}
+
+
+
+function compareChoice(userChoice) {
+    //make some logic if correct
+
+    //else make some logic if incorrect
+
+    //display next question
+
+    showQuestions()
+}
+
+startButton.addEventListener("click", startGame);
