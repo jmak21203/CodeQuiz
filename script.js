a5e252e788f5956cb16042b1a64523f57ccb99ef
Marty Hernandez <margarito.hernandezc@gmail.com>
2022-05-05 03:50:49 -0700

    updated to attempt to end quiz and show score

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ var quizInfo = document.getElementById("quizInfo");
 var choiceEl = document.getElementById("choices");
 var infoEl = document.querySelector(".questionInfo");
 var answerEl = document.getElementById("answer");
+var gameOver = document.querySelector("#gameover");
+var highScores = document.querySelector("#highscores");
+var clearScores = document.querySelector(".clear-scores");
+var highScoreButton = document.querySelector("#highscore-button");
+var playAgain = document.querySelector(".play-again");
+var submitScoreEl = document.querySelector(".score-button");
+var highScoresEl = document.querySelector(".highscores-list");
+var userName = document.querySelector("#username");
+var submitBtn = document.querySelector(".score-button");
 
 var questions = [
     {
@@ -48,8 +57,16 @@ var timer;
 var questionIndex = 0;
 var timerCount = 75;
 var questionList = document.createElement("ul");
+var time = questions.length * 15;
 
-
+function init() {
+    highScores.style.display = "none";
+    quizHeader.style.display = "show";
+    quizInfo.style.display = "show";
+    startButton.style.display = "show";
+    startButton.addEventListener("click", startGame);
+    startGame();
+}
 //function to start the game on button click
 function startGame() {
     // isWin = false;
@@ -59,6 +76,8 @@ function startGame() {
     quizHeader.style.display = "none";
     quizInfo.style.display = "none";
     startButton.style.display = "none";
+    highScores.style.display = "none";
+    gameOver.style.display = "none";
 
     showQuestions();
 }
@@ -102,6 +121,7 @@ function showQuestions() {
 
         }
         questionIndex++
+
     } else {
         gameOver()
 
@@ -136,16 +156,91 @@ function checkAnswer(buttonEl) {
     //display next question
 
     showQuestions();
+
 }
 
 function gameOver() {
-    //
+    //ends game
+
+    // // stop timer
+    clearInterval(timer);
+
+    // // show end screen
+    // var highscoreSectionEl = document.querySelector("#gameover");
+    // highscoreSectionEl.setAttribute("class", "show");
+
+    // // show final score
+    // var finalScoreEl = document.querySelector(".final-score");
+    // finalScoreEl.textContent = time;
+
+    // hide questions section
+    quizElement.style.display = "none";
+    choiceEl.style.display = "none";
 }
 
 
+function checkForEnter(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
 
+function Highscores() {
+    viewHighscoresButton.disabled = true;
+    timerElement.hidden = true;
+
+    var highscores = JSON.parse(localStorage.getItem("highscores"));
+    highscoresEl.innerHTML = "";
+    for (var i = 0; i < highscores.length; i++) {
+        var highscoreObject = highscores[i];
+        //create element to render on highscores page
+        var listItem = document.createElement("li");
+        listItem.innerText = highscoreObject.name + " - " + highscoreObject.highscore;
+        highscoresEl.appendChild(listItem);
+    }
+}
+
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+}
+
+
+clearScores.addEventListener("click", clearHighscores);
+
+
+
+// user clicks button to submit initials
 
 startButton.addEventListener("click", startGame);
+choiceEl.addEventListener("click", function (event) {
+    // ignore if target isn't a button
+    if (event.target.nodeName === "BUTTON") {
+        checkAnswer(event.target);
+    }
+});
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let highscores = JSON.parse(localStorage.getItem("highscores"));
+
+    if (highscores == null) {
+        highscores = [];
+    }
+
+    highscores.push(
+        {
+            name: document.querySelector("#name-input").value,
+            highscore: score,
+        }
+    );
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    // clear the name input field after
+    document.querySelector("#username").textContent = "";
+    renderHighscores();
+});
+
+highScoreButton.addEventListener("click", Highscores);
 choiceEl.addEventListener("click", function (event) {
     // ignore if target isn't a button
     if (event.target.nodeName === "BUTTON") {
